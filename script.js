1dd5886392935101531b0eb5334b04469e05812b
Leonardo Da Silva <ldasilva.personal@outlook.com>
2024-05-12 19:53:00 -0300

    se agrego estilo al cuadro del valor total

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const edadForm = document.getElementById('edad-form');
 const resultadoFinal = document.getElementById('resultado-final');
 const coberturasContainer = document.getElementById('coberturas-container');
 const botonCalcular = document.querySelector('.btn');
-
 const coberturas = ["Guardias", "Dentales", "Medicamentos", "Salud Mental", "Cirugías Plásticas", "Consultorios", "Análisis", "Rayos"];
 
 // Función para crear los divs con listas desplegables
@@ -12,29 +11,22 @@ function crearDivsCoberturas() {
     div.classList.add('cobertura');
     const label = document.createElement('label');
     const select = document.createElement('select');
-
     label.textContent = cobertura;
-
     const opcionNinguna = document.createElement('option');
     opcionNinguna.value = 'Ninguna';
     opcionNinguna.text = 'Ninguna';
-
     const opcionSimple = document.createElement('option');
     opcionSimple.value = 'Simple';
     opcionSimple.text = 'Simple';
-
     const opcionCompleta = document.createElement('option');
     opcionCompleta.value = 'Completa';
     opcionCompleta.text = 'Completa';
-
     select.add(opcionNinguna);
     select.add(opcionSimple);
     select.add(opcionCompleta);
     select.setAttribute('data-cobertura', cobertura);
-
     div.appendChild(label);
     div.appendChild(select);
-
     coberturasContainer.appendChild(div);
   }
 }
@@ -42,10 +34,8 @@ function crearDivsCoberturas() {
 // Función para calcular el incremento total
 function calcularIncrementoTotal(precioInicial) {
   let incrementoTotal = 0;
-
   for (const cobertura of coberturas) {
     const opcionSeleccionada = document.querySelector(`select[data-cobertura="${cobertura}"]`).value;
-
     let porcentajeIncremento;
     switch (opcionSeleccionada) {
       case "Ninguna":
@@ -58,19 +48,37 @@ function calcularIncrementoTotal(precioInicial) {
         porcentajeIncremento = 10;
         break;
     }
-
     let incremento = precioInicial * (porcentajeIncremento / 100);
     incrementoTotal += incremento;
   }
-
   return incrementoTotal;
+}
+
+// Obtener elementos del modal
+const modal = document.getElementById("modal");
+const cerrar = document.getElementsByClassName("cerrar")[0];
+
+// Función para mostrar el modal
+function mostrarModal() {
+  modal.style.display = "block";
+}
+
+// Función para ocultar el modal al hacer clic en la "x"
+cerrar.onclick = function() {
+  modal.style.display = "none";
+}
+
+// Función para ocultar el modal al hacer clic fuera del contenido
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
 
 // Función para calcular el valor total
 function calcularValorTotal() {
   const edadInput = document.getElementById('edad-input');
   let edad = parseInt(edadInput.value);
-
   if (isNaN(edad) || edad < 0 || edad > 60) {
     mostrarModal(); // Mostrar el modal si la edad no es válida
     return;
@@ -95,28 +103,12 @@ function calcularValorTotal() {
   const incrementoTotal = calcularIncrementoTotal(precioInicial);
   const valorTotal = precioInicial + incrementoTotal;
 
-  resultadoFinal.textContent = `$${valorTotal}`;
-}
-
-// Obtener elementos del modal
-const modal = document.getElementById("modal");
-const cerrar = document.getElementsByClassName("cerrar")[0];
-
-// Función para mostrar el modal
-function mostrarModal() {
-  modal.style.display = "block";
-}
-
-// Función para ocultar el modal al hacer clic en la "x"
-cerrar.onclick = function() {
-  modal.style.display = "none";
-}
-
-// Función para ocultar el modal al hacer clic fuera del contenido
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+  Swal.fire({
+    title: 'Cobertura seleccionada',
+    text: `El valor de la cobertura seleccionada es: $${valorTotal}`,
+    icon: 'success',
+    confirmButtonText: 'Aceptar'
+  });
 }
 
 // Evento para el cálculo del valor total
